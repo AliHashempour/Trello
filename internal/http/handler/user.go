23df6b50b70ebaee05403d16d847890cff6189ae
AliHashempour <ali.hashempour@gmail.com>
2024-06-12 21:26:37 -0700

    feat: implement user handler

---
+++ internal/http/handler/user.go
@@ -0,0 +1,99 @@
+package handler
+
+import (
+	"Trello/internal/hash"
+	"Trello/internal/model"
+	"Trello/internal/repository"
+	"github.com/labstack/echo/v4"
+	"net/http"
+	"strconv"
+)
+
+type User struct {
+	repo repository.UserRepository
+}
+
+func NewUser(repo repository.UserRepository) *User {
+	return &User{repo: repo}
+}
+
+func (h *User) Register(g *echo.Group) {
+	g.GET("/", h.GetUserList)
+	g.GET("/:id", h.GetUser)
+	g.POST("/", h.CreateUser)
+	g.PUT("/:id", h.UpdateUser)
+	g.DELETE("/:id", h.DeleteUser)
+}
+
+func (h *User) GetUserList(c echo.Context) error {
+	userList, err := h.repo.GetAll()
+	if err != nil {
+		return c.JSON(http.StatusInternalServerError, err)
+	}
+	return c.JSON(http.StatusOK, map[string]interface{}{"userList": userList})
+}
+
+func (h *User) GetUser(c echo.Context) error {
+	idString := c.Param("id")
+	id, err := strconv.Atoi(idString)
+	if err != nil {
+		return echo.ErrBadRequest
+	}
+
+	user, err := h.repo.GetByID(uint(id))
+	if err != nil {
+		return c.JSON(http.StatusInternalServerError, err)
+	}
+
+	return c.JSON(http.StatusOK, map[string]interface{}{"user": user})
+}
+
+func (h *User) CreateUser(c echo.Context) error {
+	var user model.User
+	if err := c.Bind(&user); err != nil {
+		return c.JSON(http.StatusInternalServerError, err)
+	}
+
+	hashedPassword := hash.PasswordHash(user.Password)
+	user.Password = hashedPassword
+
+	if err := h.repo.Create(&user); err != nil {
+		return c.JSON(http.StatusInternalServerError, err)
+	}
+	return c.JSON(http.StatusCreated, map[string]interface{}{"created": user})
+}
+
+func (h *User) UpdateUser(c echo.Context) error {
+	idString := c.Param("id")
+	id, err := strconv.Atoi(idString)
+	if err != nil {
+		return echo.ErrBadRequest
+	}
+
+	var user model.User
+	if err := c.Bind(&user); err != nil {
+		return c.JSON(http.StatusInternalServerError, err)
+	}
+
+	user.ID = uint(id)
+
+	hashedPassword := hash.PasswordHash(user.Password)
+	user.Password = hashedPassword
+
+	if err := h.repo.Update(&user); err != nil {
+		return c.JSON(http.StatusInternalServerError, err)
+	}
+	return c.JSON(http.StatusCreated, map[string]interface{}{"updated": user})
+}
+
+func (h *User) DeleteUser(c echo.Context) error {
+	idString := c.Param("id")
+	id, err := strconv.Atoi(idString)
+	if err != nil {
+		return echo.ErrBadRequest
+	}
+	if err := h.repo.Delete(uint(id)); err != nil {
+		return c.JSON(http.StatusInternalServerError, err)
+	}
+	return c.JSON(http.StatusOK, map[string]interface{}{"message": "delete user " + c.Param("id")})
+}
