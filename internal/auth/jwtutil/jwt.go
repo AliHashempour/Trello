81afec14ae00f0605ce500789c8ee22b9552ae4f
AliHashempour <ali.hashempour@gmail.com>
2024-06-13 16:10:54 -0700

    feat: jwt middleware

---
+++ internal/auth/jwtutil/jwt.go
@@ -0,0 +1,28 @@
+package jwtutil
+
+import (
+	"github.com/golang-jwt/jwt"
+	"time"
+)
+
+var JwtKey = []byte("trello-secret")
+
+type Claims struct {
+	Username string `json:"username"`
+	jwt.StandardClaims
+}
+
+func GenerateToken(username string) (string, error) {
+	expirationTime := time.Now().Add(15 * time.Minute)
+	claims := &Claims{
+		Username: username,
+		StandardClaims: jwt.StandardClaims{
+			ExpiresAt: expirationTime.Unix(),
+		},
+	}
+
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+	tokenString, err := token.SignedString(JwtKey)
+
+	return tokenString, err
+}
--- internal/http/middleware/middleware.go
+++ internal/http/middleware/middleware.go
@@ -1,8 +1,11 @@
 package middleware
 
 import (
+	"Trello/internal/auth/jwtutil"
+	"github.com/golang-jwt/jwt"
 	"github.com/labstack/echo/v4"
 	"log"
+	"net/http"
 	"time"
 )
 
@@ -15,5 +18,26 @@ func InfoLogger(next echo.HandlerFunc) echo.HandlerFunc {
 		responseTime := time.Since(requestTime)
 		log.Printf("- %s request to %s took %v\n", method, path, responseTime)
 		return err
+	}
+}
+
+func IsAuthenticatedMiddleware(next echo.HandlerFunc) echo.HandlerFunc {
+	return func(c echo.Context) error {
+		tokenString := c.Request().Header.Get("Authorization")
+
+		claims := &jwtutil.Claims{}
+
+		token, err := jwt.ParseWithClaims(tokenString, claims, func(token *jwt.Token) (interface{}, error) {
+			return jwtutil.JwtKey, nil
+		})
+		if err != nil || !token.Valid {
+			return c.JSON(http.StatusUnauthorized, map[string]string{
+				"Error": "Invalid or expired token",
+			})
+		}
+
+		c.Set("username", claims.Username)
+
+		return next(c)
 	}
 }
