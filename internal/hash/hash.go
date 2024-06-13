28722ee46121e27b461094f548a5bc01c6e1e4a7
AliHashempour <ali.hashempour@gmail.com>
2024-06-12 21:26:21 -0700

    feat: password hashing

---
+++ internal/hash/hash.go
@@ -0,0 +1,19 @@
+package hash
+
+import (
+	"golang.org/x/crypto/bcrypt"
+)
+
+func PasswordHash(password string) string {
+
+	hashedPassword, _ := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+
+	return string(hashedPassword)
+}
+
+func CheckPasswordHash(password, hash string) bool {
+
+	err := bcrypt.CompareHashAndPassword([]byte(hash), []byte(password))
+
+	return err == nil
+}
