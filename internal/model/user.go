c3ed1d6bc35cfaf8f3e4978d25b61929f0208ac1
AliHashempour <ali.hashempour@gmail.com>
2024-06-13 23:00:02 -0700

    refactor: change constraints

--- internal/model/user.go
+++ internal/model/user.go
@@ -4,7 +4,8 @@ import "gorm.io/gorm"
 
 type User struct {
 	gorm.Model
-	Username string `json:"username"  gorm:"unique;not null"`
-	Email    string `json:"email"     gorm:"unique;not null"`
-	Password string `json:"password"  gorm:"not null"`
+	Username   string       `json:"username"  gorm:"unique;not null"`
+	Email      string       `json:"email"     gorm:"unique;not null"`
+	Password   string       `json:"password"  gorm:"not null"`
+	Workspaces []*Workspace `gorm:"many2many:user_workspace_roles;"`
 }
--- internal/model/workspace.go
+++ internal/model/workspace.go
@@ -4,6 +4,7 @@ import "gorm.io/gorm"
 
 type Workspace struct {
 	gorm.Model
-	Name        string `json:"name"        gorm:"type:varchar(32);not null"`
-	Description string `json:"description" gorm:"type:text;"`
+	Name        string  `json:"name"        gorm:"type:varchar(32);not null"`
+	Description string  `json:"description" gorm:"type:text;"`
+	Users       []*User `gorm:"many2many:user_workspace_roles;"`
 }
