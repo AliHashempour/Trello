66ff0894832cacafc34df62064e771fbb3b3d166
AliHashempour <ali.hashempour@gmail.com>
2024-06-13 18:57:45 -0700

    feat: design many2many relation

--- internal/model/user.go
+++ internal/model/user.go
@@ -4,8 +4,8 @@ import "gorm.io/gorm"
 
 type User struct {
 	gorm.Model
-	Username   string      `json:"username"  gorm:"unique;not null"`
-	Email      string      `json:"email"     gorm:"unique;not null"`
-	Password   string      `json:"password"  gorm:"not null"`
-	Workspaces []Workspace `gorm:"many2many:user_workspace_roles;constraint:OnDelete:CASCADE;"`
+	Username   string       `json:"username"  gorm:"unique;not null"`
+	Email      string       `json:"email"     gorm:"unique;not null"`
+	Password   string       `json:"password"  gorm:"not null"`
+	Workspaces []*Workspace `gorm:"many2many:user_workspace_roles;constraint:OnDelete:CASCADE;"`
 }
