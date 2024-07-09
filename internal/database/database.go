c102a3980282a5b41b9673ace28c972e097778dd
AliHashempour <ali.hashempour@gmail.com>
2024-07-09 16:06:35 -0700

    chore: change host to db service

--- internal/database/database.go
+++ internal/database/database.go
@@ -7,7 +7,7 @@ import (
 )
 
 func InitializeDB() (*gorm.DB, error) {
-	dsn := "host=localhost port=5432 user=postgres dbname=trello password=postgres sslmode=disable TimeZone=Asia/Tehran"
+	dsn := "host=db port=5432 user=postgres dbname=trello password=postgres sslmode=disable TimeZone=Asia/Tehran"
 	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
 	if err != nil {
 		return nil, err
